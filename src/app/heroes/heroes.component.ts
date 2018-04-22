7e4bc9a089f5b8ec0805b5de0790dd058ac6970a
nomuyoshi <nomuyoshi0111@gmail.com>
2018-04-22 11:10:49 +0900

    mockデータの取得をserviceで行う

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,23 @@ import { Hero } from '../hero';
 })
 
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeros();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  getHeros(): void {
+    this.heroService.getHeroes()
+      .subscribe(heros => this.heroes = heros);
   }
+
 
 }
